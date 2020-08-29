f645a9052dc4ace836751feca282680fef0b722f
Greg Keeley <GregoryKeeley@pursuit.org>
2020-08-28 23:10:30 -0400

    Typealias for UICollectionviewDiffableDatasource

---
+++ Comp-Layout-Multiple-Sections/Comp-Layout-Multiple-Sections/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Comp-Layout-Multiple-Sections
+//
+//  Created by Gregory Keeley on 8/27/20.
+//  Copyright © 2020 Gregory Keeley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    typealias dataSource = UICollectionViewDiffableDataSource
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
